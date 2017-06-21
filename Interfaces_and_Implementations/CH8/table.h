dee051a19b99a44eacbfc1db5353caba061eb4b4
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2017-06-21 22:19:18 +0000

    Added Ch8 Code - Tables

---
+++ Interfaces_and_Implementations/CH8/table.h
@@ -0,0 +1,42 @@
+#ifndef TABLE_H
+#define TABLE_H
+#define T Table_T
+
+typedef struct T *T;
+
+extern t Table_new(int hint, int cmp(const void *x, const void *y),
+		unsigned hash(const void *key));
+
+extern void Table_free(T *table);
+
+extern int Table_length(T table);
+extern void *Table_put(T table, const void *key, void *value);
+extern void *Table_get(T table, const void *key);
+extern void *Table_remove(T table, const void *key);
+
+
+extern void Table_map(T table,
+		void apply(const void *key, void **value, void *cl),
+		void *cl);
+extern void **Table_toArray(T table, void *end);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#undef T
+#endif
