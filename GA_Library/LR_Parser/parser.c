7edd5c238645502b79c64b5582c24fd0172b142b
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2017-08-08 22:03:46 +0000

    Added a general plan

--- GA_Library/LR_Parser/parser.c
+++ GA_Library/LR_Parser/parser.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
 #include "parser.h"
 
-
+// development structure
+// --------------------
+// Goal: Build a LR parser
+//
+//  - configure general structure for AST
+//  - set up parser and goto tables
+//  - set up driver for parser tables
+//  - test driver
