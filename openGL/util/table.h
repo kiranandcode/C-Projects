0dfc768ec4a4c4cb938fc21c85282dfc13f07c97
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2017-09-30 19:57:03 +0000

    added hashtable implementation and cleaned dir

---
+++ openGL/util/table.h
@@ -0,0 +1,34 @@
+#ifndef TABLE_H
+#define TABLE_H
+#define T table_T
+#include "list.h"
+
+struct T;
+typedef struct T *T;
+
+struct T_iterator {
+	void *val;
+};
+
+T table_new(int hint,
+	    int cmp(const void *x, const void *y),
+	    unsigned hash(const void *key));
+void table_delete(T *table, void delete(void *));
+
+int table_length(T table);
+void *table_put(T table, const void *key, void *value);
+void *table_get(T table, const void *key);
+void *table_remove(T table, const void *key);
+void table_map(T table, void apply(const void *key, void **value, void *cl),void *cl);
+void table_print(T table, void printKey(const void *key), void printValue(void *value));
+list_L table_toList(T table);
+void table_fromList(T table, list_L keys, list_L values);
+
+struct T_iterator table_iterator(T table);
+const void *table_iteratorgetkey(struct T_iterator *iterator);
+void *table_iteratornext(struct T_iterator *iterator);
+int table_iteratorhasnext(struct T_iterator *iterator);
+
+
+#undef T
+#endif //TABLE_H
